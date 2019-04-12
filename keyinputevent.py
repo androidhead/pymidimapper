6c8a19f6c2855d5333cb6e3f4aaa926a9487aa04
Andrew.Barker <Andrew.Barker@L-PF1245LQ>
2019-04-12 15:58:08 -0400

    basic looping-exit strategy

--- keyinputevent.py
+++ keyinputevent.py
@@ -8,16 +8,22 @@ def map(input):
     else:
         return input
 
+
 #event handling
 ee = EventEmitter()
-@ee.on("myevent")
-def handler1(arg):
-   print("your input was " + map(arg))
+@ee.on("handleInput")
+def handler1(input):
+   print("your input was " + map(input))
 
 
 # use some basic keyboard input as data source to map
-keyInput = input("what's your input?")
-ee.emit("myevent", keyInput)
+run = True
+while run:
+    keyInput = input("input (type X to end): ")
+    if keyInput == "X":
+        run = False
+    else: 
+        ee.emit("handleInput", keyInput)
 
 
 
