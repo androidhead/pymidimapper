7394146c9749d41f69c22b3fd7728dd955295635
andrew barker <androidboy@gmail.com>
2019-04-14 18:36:24 -0400

    code clean up and added some notes about how I got the crazy midi ports working

--- simpleMidiInput.py
+++ simpleMidiInput.py
@@ -1,13 +1,15 @@
+#to get this working I had to do the following installs:
+#    pip install mido
+#    pip install --user --pre python-rtmidi ##(which I think provides the 'backend' for the above)
+# SOME OTHER HELPFUL CODE THAT GOT ME THERE
+#   print(mido.get_input_names())
+#   inport = mido.open_input('Babyface Midi Port 1 0') 
+
 #from mido import Message
 import mido
-
-#print(mido.get_input_names())
-
-#inport = mido.open_input('Babyface Midi Port 1 0') 
 
 with mido.open_input('Babyface Midi Port 1 0') as inport:
     for msg in inport:
         print(msg)
-
 
 
