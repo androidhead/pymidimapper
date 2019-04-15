d01d017d056506bc18a2c60c191f413dde0fadba
andrew barker <androidboy@gmail.com>
2019-04-15 10:47:46 -0400

    get simple mapper going initially

---
+++ simpleMidiNoteMapper.py
@@ -0,0 +1,18 @@
+# SOME OTHER HELPFUL CODE THAT GOT ME THERE
+#   print(mido.get_input_names())
+#   inport = mido.open_input('Babyface Midi Port 1 0') 
+# Output Ports available on my "TOWER DEV MACHINE": 
+# ['Babyface Midi Port 1 1', 'Microsoft GS Wavetable Synth 0', 'Springbeats vMIDI1 2', 'Springbeats vMIDI2 3', 'Springbeats vMIDI3 4', 'Springbeats vMIDI4 5', 'Springbeats vMIDI5 6', 'Springbeats vMIDI6 7', 'Springbeats vMIDI7 8', 'Springbeats vMIDI8 9']
+
+import mido
+
+def transform(midiMessage):
+    return midiMessage
+
+outport = mido.open_output('Springbeats vMIDI1 2')
+with mido.open_input('Babyface Midi Port 1 0') as inport:
+    for msg in inport:
+        msg = transform(msg)  #transform message
+        print(msg)
+        outport.send(msg)  #just send the message right through
+
