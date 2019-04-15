e704ae6ad6e8a9b40767d11b8c775e7afce8f5af
andrew barker <androidboy@gmail.com>
2019-04-15 10:55:58 -0400

    working midi note mapping

--- simpleMidiNoteMapper.py
+++ simpleMidiNoteMapper.py
@@ -1,18 +1,23 @@
-# SOME OTHER HELPFUL CODE THAT GOT ME THERE
-#   print(mido.get_input_names())
-#   inport = mido.open_input('Babyface Midi Port 1 0') 
-# Output Ports available on my "TOWER DEV MACHINE": 
-# ['Babyface Midi Port 1 1', 'Microsoft GS Wavetable Synth 0', 'Springbeats vMIDI1 2', 'Springbeats vMIDI2 3', 'Springbeats vMIDI3 4', 'Springbeats vMIDI4 5', 'Springbeats vMIDI5 6', 'Springbeats vMIDI6 7', 'Springbeats vMIDI7 8', 'Springbeats vMIDI8 9']
-
 import mido
 
+#you can add to these in the console with something like:
+# noteMap.update({62:61})
+noteMap = {
+    75:76,
+    77:78    
+}
+
 def transform(midiMessage):
+    midiMessage.note = noteMap.get(midiMessage.note, midiMessage.note) #map to note in noteMap.  If not found, simply pass-thru.    
     return midiMessage
 
 outport = mido.open_output('Springbeats vMIDI1 2')
 with mido.open_input('Babyface Midi Port 1 0') as inport:
     for msg in inport:
+        print("before transform:")
+        print(msg)
         msg = transform(msg)  #transform message
+        print("after transform:")
         print(msg)
         outport.send(msg)  #just send the message right through
 
--- simpleMidiPassthru.py
+++ simpleMidiPassthru.py
@@ -1,9 +1,3 @@
-# SOME OTHER HELPFUL CODE THAT GOT ME THERE
-#   print(mido.get_input_names())
-#   inport = mido.open_input('Babyface Midi Port 1 0') 
-# Output Ports available on my "TOWER DEV MACHINE": 
-# ['Babyface Midi Port 1 1', 'Microsoft GS Wavetable Synth 0', 'Springbeats vMIDI1 2', 'Springbeats vMIDI2 3', 'Springbeats vMIDI3 4', 'Springbeats vMIDI4 5', 'Springbeats vMIDI5 6', 'Springbeats vMIDI6 7', 'Springbeats vMIDI7 8', 'Springbeats vMIDI8 9']
-
 import mido
 
 outport = mido.open_output('Springbeats vMIDI1 2')
