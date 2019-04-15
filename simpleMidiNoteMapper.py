b1b5f8c5f64c7b563283fd0a6096891d7806e760
andrew barker <androidboy@gmail.com>
2019-04-15 17:53:00 -0400

    get config file working

--- simpleMidiNoteMapper.py
+++ simpleMidiNoteMapper.py
@@ -5,15 +5,21 @@ print("INPUT PORTS:")
 inputs = mido.get_input_names()
 print(mido.get_input_names())
 print("")
-
 print("OUTPUT PORTS:")
 outputs = mido.get_output_names()
 print(mido.get_output_names())
 print("")
 
+#get config
+import json
+with open('config.json', 'r') as f:
+    config = json.load(f)
+    inportName = config["INPUT_PORT"]
+    outportName = config["OUTPUT_PORT"]
 
-#you can add to these in the console with something like:
-# noteMap.update({62:61})
+# the actual note maps.  probably should be loaded via file(s) at some point
+#   you can add to these in the console with something like:
+#   noteMap.update({62:61})
 noteMap = {
     75:76,
     77:78    
@@ -23,8 +29,8 @@ def transform(midiMessage):
     midiMessage.note = noteMap.get(midiMessage.note, midiMessage.note) #map to note in noteMap.  If not found, simply pass-thru.    
     return midiMessage
 
-outport = mido.open_output('Springbeats vMIDI1 2')
-with mido.open_input('Babyface Midi Port 1 0') as inport:
+outport = mido.open_output(outportName)
+with mido.open_input(inportName) as inport:
     for msg in inport:
         print("before transform:")
         print(msg)
@@ -33,3 +39,4 @@ with mido.open_input('Babyface Midi Port 1 0') as inport:
         print(msg)
         outport.send(msg)  #just send the message right through
 
+ 
