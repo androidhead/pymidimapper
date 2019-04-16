e7006c1b206c40d07f99f22c857172e95dc4083c
andrew barker <androidboy@gmail.com>
2019-04-15 20:07:51 -0400

    get note map to be driven by config file

--- simpleMidiNoteMapper.py
+++ simpleMidiNoteMapper.py
@@ -1,29 +1,28 @@
 import mido
 
 #show which ports are available.
-print("INPUT PORTS:")
+print('INPUT PORTS:')
 inputs = mido.get_input_names()
 print(mido.get_input_names())
-print("")
-print("OUTPUT PORTS:")
+print('')
+print('OUTPUT PORTS:')
 outputs = mido.get_output_names()
 print(mido.get_output_names())
-print("")
+print('')
 
 #get config
 import json
 with open('config.json', 'r') as f:
     config = json.load(f)
-    inportName = config["INPUT_PORT"]
-    outportName = config["OUTPUT_PORT"]
+    inportName = config['INPUT_PORT']
+    outportName = config['OUTPUT_PORT']
 
-# the actual note maps.  probably should be loaded via file(s) at some point
-#   you can add to these in the console with something like:
-#   noteMap.update({62:61})
-noteMap = {
-    75:76,
-    77:78    
-}
+#import note map from config file
+noteMap = {}
+allNoteRoutes = config['NOTE_MAP']
+for noteRoute in config['NOTE_MAP']:
+    print('load route:', noteRoute, allNoteRoutes[noteRoute])
+    noteMap.update({int(noteRoute): allNoteRoutes[noteRoute]}) #add to noteMap
 
 def transform(midiMessage):
     if hasattr(midiMessage, 'note'):
@@ -33,10 +32,10 @@ def transform(midiMessage):
 outport = mido.open_output(outportName)
 with mido.open_input(inportName) as inport:
     for msg in inport:
-        print("before transform:")
+        print('before transform:')
         print(msg)
         msg = transform(msg)  #transform message
-        print("after transform:")
+        print('after transform:')
         print(msg)
         outport.send(msg)  #just send the message right through
 
