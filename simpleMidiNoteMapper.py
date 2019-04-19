87ec3b8819dc6c9a2bfb573bf46f1f670e0737d3
andrew barker <androidboy@gmail.com>
2019-04-19 15:50:07 -0400

    baseline for current so can check history

--- simpleMidiNoteMapper.py
+++ simpleMidiNoteMapper.py
@@ -1,57 +1,48 @@
 import mido
-import mido.backends.rtmidi
+#import mido.backends.rtmidi
+import json
 
+def transform(midiMessage):
+    if hasattr(midiMessage, 'note'):
+        midiMessage.note = noteMap.get(midiMessage.note, midiMessage.note) #map to note in noteMap.  If not found, simply pass-thru.        
+    return midiMessage
 
-try:
-    inputs = mido.get_input_names()
-    input("Press Enter to terminate.")    
+#try:
 
-except Exception as e:
-    print(e)
-    input("Broke.  Press enter to terminate.")
+#show which ports are available.
+print('INPUT PORTS:')
+inputs = mido.get_input_names()
+print(mido.get_input_names())
+print('')
+print('OUTPUT PORTS:')
+outputs = mido.get_output_names()
+print(mido.get_output_names())
+print('')
 
-# #show which ports are available.
-# print('INPUT PORTS:')
-# inputs = mido.get_input_names()
-# print(mido.get_input_names())
-# print('')
-# print('OUTPUT PORTS:')
-# outputs = mido.get_output_names()
-# print(mido.get_output_names())
-# print('')
+#get config    
+with open('config.json', 'r') as f:
+    config = json.load(f)
+    inportName = config['INPUT_PORT']
+    outportName = config['OUTPUT_PORT']
 
+#import note map from config file
+noteMap = {}
+allNoteRoutes = config['NOTE_MAP']
+for noteRoute in config['NOTE_MAP']:
+    print('load route:', noteRoute, allNoteRoutes[noteRoute])
+    noteMap.update({int(noteRoute): allNoteRoutes[noteRoute]}) #add to noteMap
 
+#todo: make sure midi devices are closed before program ends?
+outport = mido.open_output(outportName)
+with mido.open_input(inportName) as inport:
+    for msg in inport:
+        print('before transform:', msg)
+        msg = transform(msg)  #transform message
+        print('after transform:', msg)        
+        #outport.send(msg)  #just send the message right through                
 
-# #get config
-# import json
-# with open('config.json', 'r') as f:
-#     config = json.load(f)
-#     inportName = config['INPUT_PORT']
-#     outportName = config['OUTPUT_PORT']
+# except Exception as e:
+#     print('Error::')
+#     print(e)
 
-# #import note map from config file
-# noteMap = {}
-# allNoteRoutes = config['NOTE_MAP']
-# for noteRoute in config['NOTE_MAP']:
-#     print('load route:', noteRoute, allNoteRoutes[noteRoute])
-#     noteMap.update({int(noteRoute): allNoteRoutes[noteRoute]}) #add to noteMap
-
-# def transform(midiMessage):
-#     if hasattr(midiMessage, 'note'):
-#         midiMessage.note = noteMap.get(midiMessage.note, midiMessage.note) #map to note in noteMap.  If not found, simply pass-thru.        
-#     return midiMessage
-
-# try:
-#     outport = mido.open_output(outportName)
-#     with mido.open_input(inportName) as inport:
-#         for msg in inport:
-#             print('before transform:', msg)
-#             msg = transform(msg)  #transform message
-#             print('after transform:', msg)        
-#             outport.send(msg)  #just send the message right through                
-## todo: try except Exception as e:  instead of the code below. Then you can do print(e) to get better reporting.  
-# except:
-#     print('something broke.  It is likely that the specified midi in port or midi out port does not exist.')
-#     input("Press Enter to terminate.")
-
-# #todo: make sure midi devices are closed?
+# input('Press Enter to terminate.')
