448da9a5a0d3e8a05f2228facd89a8a922a0b80e
andrew barker <androidboy@gmail.com>
2019-04-19 16:01:29 -0400

    baseline working as exe (when babyface pro connected)

--- simpleMidiNoteMapper.py
+++ simpleMidiNoteMapper.py
@@ -1,5 +1,5 @@
 import mido
-#import mido.backends.rtmidi
+import mido.backends.rtmidi
 import json
 
 def transform(midiMessage):
@@ -7,42 +7,41 @@ def transform(midiMessage):
         midiMessage.note = noteMap.get(midiMessage.note, midiMessage.note) #map to note in noteMap.  If not found, simply pass-thru.        
     return midiMessage
 
-#try:
+try:
+    #show which ports are available.
+    print('INPUT PORTS:')
+    inputs = mido.get_input_names()
+    print(mido.get_input_names())
+    print('')
+    print('OUTPUT PORTS:')
+    outputs = mido.get_output_names()
+    print(mido.get_output_names())
+    print('')
 
-#show which ports are available.
-print('INPUT PORTS:')
-inputs = mido.get_input_names()
-print(mido.get_input_names())
-print('')
-print('OUTPUT PORTS:')
-outputs = mido.get_output_names()
-print(mido.get_output_names())
-print('')
+    #get config    
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+        inportName = config['INPUT_PORT']
+        outportName = config['OUTPUT_PORT']
 
-#get config    
-with open('config.json', 'r') as f:
-    config = json.load(f)
-    inportName = config['INPUT_PORT']
-    outportName = config['OUTPUT_PORT']
+    #import note map from config file
+    noteMap = {}
+    allNoteRoutes = config['NOTE_MAP']
+    for noteRoute in config['NOTE_MAP']:
+        print('load route:', noteRoute, allNoteRoutes[noteRoute])
+        noteMap.update({int(noteRoute): allNoteRoutes[noteRoute]}) #add to noteMap
 
-#import note map from config file
-noteMap = {}
-allNoteRoutes = config['NOTE_MAP']
-for noteRoute in config['NOTE_MAP']:
-    print('load route:', noteRoute, allNoteRoutes[noteRoute])
-    noteMap.update({int(noteRoute): allNoteRoutes[noteRoute]}) #add to noteMap
+    #todo: make sure midi devices are closed before program ends?
+    outport = mido.open_output(outportName)
+    with mido.open_input(inportName) as inport:
+        for msg in inport:
+            print('before transform:', msg)
+            msg = transform(msg)  #transform message
+            print('after transform:', msg)        
+            #outport.send(msg)  #just send the message right through                
 
-#todo: make sure midi devices are closed before program ends?
-outport = mido.open_output(outportName)
-with mido.open_input(inportName) as inport:
-    for msg in inport:
-        print('before transform:', msg)
-        msg = transform(msg)  #transform message
-        print('after transform:', msg)        
-        #outport.send(msg)  #just send the message right through                
+except Exception as e:
+     print('Error::')
+     print(e)
 
-# except Exception as e:
-#     print('Error::')
-#     print(e)
-
-# input('Press Enter to terminate.')
+input('Press Enter to terminate.')
